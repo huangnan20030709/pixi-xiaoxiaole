7d7931ecad7818b25e407f359f51c67952ede905
huangnan <huangnan@huya.com>
2024-11-08 17:59:44 +0800

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+有问题删除 node_modules,删除 eslint.config.js
+
+或手动执行命令
+
+```ts
+pnpm i eslint-plugin-react
+```
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function App() {
+  const a = 1;
+  return <>app</>;
+}
+
+export default App;
